2fa6b78666b0d492b95cdbe8f87ef8e6133e679f
xyiakacobaka <theneetfreekgames@gmail.com>
2024-06-21 02:12:57 +0300

    Подключил обращение к двум апи. Поиск гео. характеристик города и его погоду на 5 дней

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
-function MyComponent() {
-  fetch("https://api.openweathermap.org/geo/1.0/direct?q=Moskow&limit=5&appid=e65a15061c79d126ccd4123c097fedba")
-  .then(response => response.json())
-  .then(data => console.log(data));
+function City(){
+  const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=Москва&limit=5&appid=e65a15061c79d126ccd4123c097fedba`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=e65a15061c79d126ccd4123c097fedba`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    });
 }
 
 root.render(
   <div id='cityContainer'>
-    <input type="city" placeholder="Погода какого города вас интересует?"/>
+    <City />
+    <input type='text'  placeholder='Погода какого города вас интересует?' id='input' oninput={console.log('input'.value)}/>    
   </div>
 );
