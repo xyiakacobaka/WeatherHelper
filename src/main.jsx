b7313cfeb0a922ef260db03d544d83eb029a9bff
xyiakacobaka <theneetfreekgames@gmail.com>
2024-06-21 15:59:41 +0300

    Реализовано событие onInput элемента <input>. Реализован динамически изменяемый Request API

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
-function City(){
-  const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=Москва&limit=5&appid=e65a15061c79d126ccd4123c097fedba`;
+function City(props){
+  const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${props}&limit=5&appid=e65a15061c79d126ccd4123c097fedba`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=e65a15061c79d126ccd4123c097fedba`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=e65a15061c79d126ccd4123c097fedba`)
+        .then(response => response.json())
+        .then((data) => {console.log(data)})
     });
 }
 
+function Counter() {
+  const [value, setValue] = useState('');
+
+  let ref = useRef("");
+
+  const cityInput = (props) => { setValue(props.target.value); City(props.target.value);}
+
+  return <input type='text'  placeholder='Погода какого города вас интересует?' id='cityContainer' value={value} onInput={cityInput}/>;
+}
+
 root.render(
-  <div id='cityContainer'>
-    <City />
-    <input type='text'  placeholder='Погода какого города вас интересует?' id='input' oninput={console.log('input'.value)}/>    
+  <div id='searchLine'>
+    <Counter/>    
   </div>
 );
